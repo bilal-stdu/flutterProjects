9917083765621940807d552286bb168882d840ca
bilal-stdu <mbilalahmed977@gmail.com>
2023-11-17 11:11:40 +0500

    upload UI

---
+++ Mad project/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:extract_images/screens/UploadScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  String tit = 'Upload File';
+  String sub = 'Browse and choose the files you want to upload.';
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter File upload Demo',
+      home: UploadScreen(1, tit, sub),
+    );
+  }
+}
+
---
+++ Mad project/lib/screens/UploadScreen.dart
@@ -0,0 +1,91 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class UploadScreen extends StatefulWidget {
+  final int index;
+  final String title;
+  final String subtitle;
+
+  UploadScreen(this.index,this.title, this.subtitle);
+
+  @override
+  State<UploadScreen> createState() => _UploadScreenState();
+}
+
+class _UploadScreenState extends State<UploadScreen> {
+  FilePickerResult? result;
+  String tit = 'Uploading....';
+
+  String sub = 'Please wait a moment while we upload your files.';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body:  Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(
+            height: 60.0,
+          ),
+          Text(
+            widget.title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 40.5,
+            ),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Text(
+              widget.subtitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.5,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          FloatingActionButton(
+            onPressed: () async {
+              result = await FilePicker.platform.pickFiles(
+                type: FileType.custom,
+                allowedExtensions: ['pdf'],
+              );
+              if (result == null) {
+                print("No file selected");
+              } else{
+                setState(() {
+                  for (var element in result!.files) {
+                    print(element.name);
+                  }
+                });
+              }
+            },
+            backgroundColor: Colors.amber,
+            child: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+          )
+
+        ],
+      ),
+
+    );
+
+  }
+}
